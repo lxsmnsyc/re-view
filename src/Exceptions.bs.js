f410aad03cec929f15d4c8109926888e91b6b0a7
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-05-22 13:48:32 +0800

    Add missing exceptions

--- src/Exceptions.bs.js
+++ src/Exceptions.bs.js
@@ -5,6 +5,12 @@ var Caml_exceptions = require("bs-platform/lib/js/caml_exceptions.js");
 
 var MissingBasicComponentConstructor = Caml_exceptions.create("Exceptions-ReView.MissingBasicComponentConstructor");
 
+var MissingComponentConstructor = Caml_exceptions.create("Exceptions-ReView.MissingComponentConstructor");
+
+var MissingMemoComponentConstructor = Caml_exceptions.create("Exceptions-ReView.MissingMemoComponentConstructor");
+
+var MissingMemoBasicComponentConstructor = Caml_exceptions.create("Exceptions-ReView.MissingMemoBasicComponentConstructor");
+
 var OutOfContextHookCall = Caml_exceptions.create("Exceptions-ReView.OutOfContextHookCall");
 
 var IncompatibleHook = Caml_exceptions.create("Exceptions-ReView.IncompatibleHook");
@@ -18,6 +24,9 @@ var UnboundContextInstance = Caml_exceptions.create("Exceptions-ReView.UnboundCo
 var InvalidHostConstructor = Caml_exceptions.create("Exceptions-ReView.InvalidHostConstructor");
 
 exports.MissingBasicComponentConstructor = MissingBasicComponentConstructor;
+exports.MissingComponentConstructor = MissingComponentConstructor;
+exports.MissingMemoComponentConstructor = MissingMemoComponentConstructor;
+exports.MissingMemoBasicComponentConstructor = MissingMemoBasicComponentConstructor;
 exports.OutOfContextHookCall = OutOfContextHookCall;
 exports.IncompatibleHook = IncompatibleHook;
 exports.MissingContext = MissingContext;
